16eb5b91fbede3dbffdf9967031df3a76f672e5d
FLayVe <flayve.dev@gmail.com>
2024-04-23 04:03:17 +0300

    Debug wellcome massage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-const tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
 
-document.getElementById("welcome__user-name").innerText = ('Hello, %s', tg.initDataUnsafe.first_name)
+document.getElementById("welcome__user-name").innerText = `Hello, ${tg.initDataUnsafe.first_name}`;
