4f54a52179badaa109f836dd0abfa1c424dd9343
FLayVe <flayve.dev@gmail.com>
2024-04-23 04:22:20 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ let tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-let wellcome_message =  document.getElementById("welcome__user-name");
+let wellcome_message = document.getElementById("welcome__user-name");
 
-wellcome_messagem.innerText  = `Hi, ${tg.initDataUnsafe.first_name}`;
+wellcome_message.innerText  = `Hi, ${tg.initDataUnsafe.first_name}`;
