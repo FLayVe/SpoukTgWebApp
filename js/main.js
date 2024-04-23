3db3e8306ec0d08dbcedce9a090736974d4218e8
FLayVe <flayve.dev@gmail.com>
2024-04-23 09:24:59 +0300

    optimization Form. test userData.usename

--- js/main.js
+++ js/main.js
@@ -2,35 +2,36 @@ let tg = window.Telegram.WebApp;
 const userData = tg.initDataUnsafe.user;
 
 const token = '6419070785:AAHaIp5r0-T5xsjdghyuq-6i2WB0J6Q3bOY'
-const groupChatID  = '-1002123032129'
+const groupChatId  = '-1002123032129'
 const botApiUrl = `https://api.telegram.org/bot${token}/sendMessage`;
 
 window.onload = function(){
 
-    document.getElementById("user-name").innerText = userData.first_name;
+    document.getElementById("user-name").innerText = userData.username;
 
 }
 
-function sendOrder(formName, phoneId) {
+function sendOrder() {
 
+    var teg = "{User}"
     var name = document.getElementById('name').value;
-    var phone = document.getElementById(phoneId).value;
+    var phone = document.getElementById('phone').value;
     var comment = document.getElementById('comment').value;
-
+    
     var message = `
-        ${formName}:
-        Username: ${userData.username}
-        Name: ${name}
-        Phone: ${phone}
-        Comment: ${comment} 
+        ${teg}
+      Name: ${name}
+      Phone: ${phone}
+      Link network: ${comment}
     `;
 
+    
     var params = {
-        chat_id: groupChatID,
+        chat_id: groupChatId,
         text: message,
     };
 
-    aaxios.post(botApiUrl, params)
+    axios.post(botApiUrl, params)
         .then(function (response) {
             console.log(response);
             alert('Повідомлення відправлено успішно!');
@@ -41,8 +42,8 @@ function sendOrder(formName, phoneId) {
         });
 
     document.getElementById('name').value = '';
-    document.getElementById(phoneId).value = '';
+    document.getElementById('phone').value = '';
     document.getElementById('comment').value = '';
 
-    return false;  
+    return false;
 }
