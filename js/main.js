63f68cefda6f17bf0ddfa5412069d77c07418bd1
FLayVe <flayve.dev@gmail.com>
2024-04-23 05:01:58 +0300

    modify wellcome text

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let tg = window.Telegram.WebApp;
-
-tg.ready();
-tg.expand();
-
-const userData = tg.initDataUnsafe.user;
-const username = userData.username ? `@${userData.username}` : userData.first_name;
-
-document.getElementById("welcome__user-name").innerText = `Привіт, ${username}!`;
